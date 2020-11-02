188f70265e6feba4877fc4cb48c4d11dac49058c
Ashley Pean <pean.ashley@gmail.com>
2020-11-01 20:15:07 -0500

    Navbar structure and styling set up

---
+++ src/components/Nav.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom'
+import '../styles/Navbar.css'
+
+export default function Nav() {
+  return (
+    <nav>
+      <NavLink to="/" activeClassName="selected-nav-item" >
+        Home
+      </NavLink>
+
+      <NavLink to="/instructions" activeClassName="selected-nav-item" >
+        Instructions
+      </NavLink>
+
+      <NavLink to="/take" activeClassName="selected-nav-item" >
+        Take a Quiz
+      </NavLink>
+    </nav>
+  )
+}
