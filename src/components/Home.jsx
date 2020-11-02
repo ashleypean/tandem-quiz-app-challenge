1d9f02abc80bda3cf79a11ecde98876bfc9e68f1
Ashley Pean <pean.ashley@gmail.com>
2020-11-01 20:18:36 -0500

    Home page structure and styling completed

---
+++ src/components/Home.jsx
@@ -0,0 +1,13 @@
+import '../styles/Home.css'
+import React from 'react'
+import Nav from './Navbar'
+
+export default function Home() {
+
+  return (
+    <div className="home">
+        <Nav />
+        <h1>QUIZZLY</h1>
+    </div>
+  )
+}
